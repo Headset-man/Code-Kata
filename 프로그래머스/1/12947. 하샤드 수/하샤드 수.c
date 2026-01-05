528940bf87f8f55446b8fc3d82f726123f99cb93
Headset-man <headset.on.aza@gmail.com>
2026-01-05 09:12:14 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool solution(int x) {
+    int sum = 0;
+    int temp = x;
+    
+    while (temp > 0)
+    {
+        sum += temp % 10;
+        temp /= 10;
+    }
+    
+    return (x % sum == 0);
+}
