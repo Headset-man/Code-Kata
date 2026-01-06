4309c41012c772b462783aba7d2ddd2c8803b7ff
Headset-man <headset.on.aza@gmail.com>
2026-01-06 09:36:15 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.39 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num) {
+    long long n = num;
+    int count = 0;
+
+    if (n == 1) return 0;
+
+    while (n != 1 && count < 500) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = n * 3 + 1;
+        }
+        count++;
+    }
+
+    return (n == 1) ? count : -1;
+}
+
